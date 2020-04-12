16fc335880dbc568a4527a8ac1845ff29f6890e9
Seth <s.zim517@gmail.com>
2020-04-12 16:25:03 -0400

    Adds initial clientside SignalR stuffs for creating a new game

--- ClientApp/src/components/StartGame.js
+++ ClientApp/src/components/StartGame.js
@@ -1,25 +1,46 @@
 import { Input, Label, Form, Button } from 'reactstrap';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as signalR from '@aspnet/signalr';
 
 const StartGame = (props) => {
+    let connection = null;
+    const startingGame = props.start;
     const [username, setUsername] = useState('');
+    const [roomCode, setRoomCode] = useState('');
 
-    const submitForm = () => {
+    useEffect(() => {
+        // connection = new signalR.HubConnectionBuilder()
+        //     .withUrl("/gameHub")
+        //     .configureLogging(signalR.LogLevel.Information)
+        //     .build();
+
+        // connection.start();
+
+    }, []);
+
+    const submitForm = (e) => {
+        e.preventDefault();
         console.log('Submitting with name: ', username);
+        connection = new signalR.HubConnectionBuilder()
+            .withUrl("/gameHub")
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+
+        connection.start();
     }
-    
+
     return (
         <div>
-            <Form onSubmit={submitForm}>
+            <Form onSubmit={(e) => submitForm(e)}>
                 <Label for="username">Name</Label>
                 <Input type="text" name="username" onChange={(e) => setUsername(e.target.value)} />
-                {(props.start 
+                {(startingGame 
                     ? null 
                     : (<div>
                         <Label for="username">Room Code</Label>
-                        <Input type="text" name="roomCode" />
+                        <Input type="text" name="roomCode" onChange={(e) => setRoomCode(e.target.value)} />
                         </div>) )}
-                <Button type="submit">Submit</Button>
+                <Button onClick={submitForm}>Submit</Button>
             </Form>
         </div>
     )
